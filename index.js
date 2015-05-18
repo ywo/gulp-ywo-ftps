512795471ccc3be450bc9c0870f6430f55965fbd
ratly <lyj520@gmail.com>
2015-05-19 00:54:53 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var pp = require('preprocess');
+var FTPS = require('ywo-ftps');
+var _ = require('lodash');
+var PATH = require('path');
+
+const PLUGIN_NAME = require('./package.json').name;
+var defaults = {
+    host: '127.0.0.1',
+    username: 'root',
+    password: '***',
+    protocol: 'sftp',
+    root : '/home/ywo',
+    port: 22,
+}
+module.exports = function (options) {
+    var cfg = _.merge({}, defaults, options);
+    var ftps = new FTPS(cfg);
+    return through.obj(function (file, enc, cb) {
+        var _this = this;
+        if (file.isNull()) {
+            _this.push(file);
+            return cb();
+        }
+        var relativePath = cfg.localPath.replace(cfg.localRoot, '')
+        var remotePath = PATH.join(cfg.remote_path, relativePath);
+        ftps.put(cfg.localPath, remotePath).exec(function(err, result){
+            if(result.error) {
+                new gutil.PluginError(PLUGIN_NAME, result.error);
+            } else {
+                gutil.log('send success', relativePath);
+            }
+            ftps = null;
+            _this.push(file);
+            cb();
+        });
+    });
+};
